7d49e144d2e2ebc678e69f6b9fd92fc82192fb64
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-04-28 00:55:59 +0300

    feat : deployment time

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,40 @@
-'use client'
-import {Amatic_SC} from 'next/font/google';
-import {Ranchers} from 'next/font/google';
+"use client";
+import { Amatic_SC, Ranchers } from "next/font/google";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Toaster } from "react-hot-toast";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
-import { Metadata } from 'next';
-import Head from 'next/head';
-import 'swiper/css';
-import 'swiper/css/autoplay';
+import "swiper/css";
+import "swiper/css/autoplay";
 import "./globals.css";
 
-config.autoAddCss = false; 
+config.autoAddCss = false;
+
+const metadata = {
+  title: "Burrito",
+  description: "this is our mexican restaurant website",
+  icons: {
+    icon: "/icon.png",
+  },
+};
 
 export const headers = Ranchers({
-  weight: ['400'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const normal = Amatic_SC({
-  weight: ['400', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
-})
-
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -39,22 +43,19 @@ export default function RootLayout({
 }>) {
   useEffect(() => {
     AOS.init({
-      duration: 1000, 
-      once: true,     
+      duration: 1000,
+      once: true,
     });
   }, []);
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="icon.png"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <body className={`${normal.className} text-[20px] capitalize bg-secondary-filler-color overflow-x-hidden`}>
+      <body
+        className={`${normal.className} text-[20px] capitalize bg-secondary-filler-color overflow-x-hidden`}
+      >
         <Nav />
         {children}
         <Footer />
         <Toaster />
-        {AOS.init()}
       </body>
     </html>
   );
